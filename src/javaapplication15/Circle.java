49a5ff6981436b1319578c8e26c61676e899fdc9
a <a@a.com>
2021-09-19 19:32:44 +0300

    Circle commit

---
+++ src/javaapplication15/Circle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication15;
+
+/**
+ *
+ * @author Kostas
+ */
+public class Circle {
+    private int x;
+    private int y;
+    private int r;
+
+    public Circle(int x, int y, int r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    
+    
+    
+}
